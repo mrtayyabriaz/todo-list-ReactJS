cd329d9a9ce8a06645b39c9a934202ef035cf737
Tayyab Riaz <92810755+mrtayyabriaz@users.noreply.github.com>
2023-12-16 04:25:21 +0000

    Emptying title and description after todo added..

--- src/MyCompanents/AddToDo.js
+++ src/MyCompanents/AddToDo.js
@@ -1,34 +1,34 @@
 import React, { useState } from 'react'
 
 
-export default function AddToDo({addTodo}) {
-    const [Title, setTitle] = useState("");
-    const [Desc, setDesc] = useState("");
+export default function AddToDo({ addTodo }) {
+  const [Title, setTitle] = useState("");
+  const [Desc, setDesc] = useState("");
 
-    const submit = (e) => {
-        e.preventDefault();
-        if (!Title || !Desc) {
-            alert("Title or Description could not be empty.");
-        }else{
-            addTodo(Title, Desc);
-            // setTitle("");
-            // setDesc("");
-        }
+  const submit = (e) => {
+    e.preventDefault();
+    if (!Title || !Desc) {
+      alert("Title or Description could not be empty.");
+    } else {
+      addTodo(Title, Desc);
+      setTitle("");
+      setDesc("");
     }
-    return (
-        <div className="container my-3">
-            <h2 className="text-center w-100">Add Todo</h2>
-            <form onSubmit={submit}>
-                <div className="mb-3">
-                    <label htmlFor="ToDoTitle" className="form-label">ToDo Title</label>
-                    <input type="text" value={Title} onChange={(e) => setTitle(e.target.value)} className="form-control" id="ToDoTitle" aria-describedby="emailHelp" />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="ToDoDesc" className="form-label">Description</label>
-                    <input type="text" value={Desc} onChange={(e) => setDesc(e.target.value)} className="form-control" id="ToDoDesc" />
-                </div>
-                <button type="submit" className="btn btn-success btn-sm">Add</button>
-            </form>
+  }
+  return (
+    <div className="container my-3">
+      <h2 className="text-center w-100">Add Todo</h2>
+      <form onSubmit={submit}>
+        <div className="mb-3">
+          <label htmlFor="ToDoTitle" className="form-label">ToDo Title</label>
+          <input type="text" value={Title} onChange={(e) => setTitle(e.target.value)} className="form-control" id="ToDoTitle" aria-describedby="emailHelp" />
         </div>
-    )
+        <div className="mb-3">
+          <label htmlFor="ToDoDesc" className="form-label">Description</label>
+          <input type="text" value={Desc} onChange={(e) => setDesc(e.target.value)} className="form-control" id="ToDoDesc" />
+        </div>
+        <button type="submit" className="btn btn-success btn-sm">Add</button>
+      </form>
+    </div>
+  )
 }
